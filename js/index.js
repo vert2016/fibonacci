9f499493285193804d15f37216d6e47ae71e6561
marta bobryk <marta.bzl@gmail.com>
2016-11-10 16:08:31 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+     for (i=1; i < 10; i++){
+          var i=1;
+        var a=1;
+        var b=0;
+        var f=a+b;
+        f=a+b;
+        a=b;
+        b=f;
+        document.write(f);
+    }
+       
+   
+   
